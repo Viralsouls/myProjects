9b87ddfff5a6a07f68bd7158281eebdbb524eb04
Viralsouls <96252678+Viralsouls@users.noreply.github.com>
2024-02-19 14:25:54 -0500

    Create working_with_numbers.py

---
+++ working_with_numbers.py
@@ -0,0 +1,9 @@
+typed = 0
+
+while True:
+    number = int(input("Please type in integer numbers. Type in 0 to finish.")
+    typed += 1
+
+    if number == 0:
+        break
+print("Numbers typed in", typed)
